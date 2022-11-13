137639e57a09e88846d910df0ddc1fb32c98c140
yuriko-aya <thechildoflily@gmail.com>
2022-11-13 09:57:25 +0700

    ADD v2

--- src/js/genpass.js
+++ src/js/genpass.js
@@ -11,6 +11,19 @@ function make_password(length) {
     return result;
 }
 
+function make_password_v2(length) {
+    var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789~!@#$%^&*()_-+={[}]|\:;<,>.?/';
+    var result           = '';
+    var charactersLength = characters.length;
+    for (var i = 0; i < length; i++) {
+        if ((i % 8 == 0) && (i != 0)) {
+            result += '-'
+        }
+        result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    return result;
+}
+
 function copy_password() {
     var copyText = document.getElementById("generated_password");
     var cleaned_password = decode_html(copyText.innerHTML)
@@ -40,6 +53,26 @@ function generate_password() {
     document.getElementById("generated_password").classList.add('display-3')
     document.getElementById("copy-button").style.display = 'inline-block'
     document.getElementById("generated_password").innerHTML = escape_html(result)
+    document.getElementById("main-button").style.display = 'none'
+    document.getElementById("left-button").style.display = 'inline-block'
+}
+
+function generate_password_v2() {
+    var regex = /^(?=.*\d)(?=.*[!@#$%^&*])(?=.*[a-z])(?=.*[A-Z]).{8,}$/;;
+    while (true) {
+        var result = make_password_v2(15);
+        if (regex.test(result)) {
+            break;
+        }
+    }
+    first_char = make_password(1)
+    console.log(result)
+    console.log(first_char)
+    var full_result = first_char + result
+    document.getElementById("generated_password").classList.add('bg-info')
+    document.getElementById("generated_password").classList.add('display-3')
+    document.getElementById("copy-button").style.display = 'inline-block'
+    document.getElementById("generated_password").innerHTML = escape_html(full_result)
     document.getElementById("main-button").style.display = 'none'
     document.getElementById("left-button").style.display = 'inline-block'
 }
