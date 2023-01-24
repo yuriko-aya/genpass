fa6084b25616196c3025eaeaf253803714791e0e
yuriko-aya <thechildoflily@gmail.com>
2023-01-24 08:59:52 +0700

    FIX api

--- src/js/genpass.js
+++ src/js/genpass.js
@@ -15,10 +15,12 @@ function make_password_v2(length) {
     var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789~!@#$%^&*()_-+={[}]|\:;<,>.?/';
     var result = '';
     var characters_length = characters.length;
-    for (var i = 0; i < length; i++) {
+    first_char = make_password(1)
+    for (var i = 0; i < length - 1; i++) {
         result += characters.charAt(Math.floor(Math.random() * characters_length));
     }
-    return result;
+    var full_result = first_char + result
+    return full_result;
 }
 
 function copy_password() {
@@ -57,13 +59,11 @@ function generate_password() {
 function generate_password_v2() {
     var regex = /^(?=.*\d)(?=.*[!@#$%^&*])(?=.*[a-z])(?=.*[A-Z]).{8,}$/;;
     while (true) {
-        var result = make_password_v2(15);
+        var result = make_password_v2(16);
         if (regex.test(result)) {
             break;
         }
     }
-    first_char = make_password(1)
-    var full_result = first_char + result
     document.getElementById("generated_password").classList.add('bg-info')
     document.getElementById("generated_password").classList.add('display-3')
     document.getElementById("copy-button").style.display = 'inline-block'
